8de445e584b7f1ddc518432b642f5f3396949356
PJMan7 <PJMan7@whatever.com>
2025-11-22 14:43:16 +0000

    chipnet

--- packages/dapp/src/app/providers.tsx
+++ packages/dapp/src/app/providers.tsx
@@ -4,7 +4,7 @@ import { Web3ModalConnectorContextProvider } from "@bch-wc2/web3modal-connector"
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <Web3ModalConnectorContextProvider config={{projectId: 'b89a472ca74470463e1c16f3f6bfba4f'}}>
+    <Web3ModalConnectorContextProvider config={{projectId: 'b89a472ca74470463e1c16f3f6bfba4f', useChipnet: true}}>
       {children}
     </Web3ModalConnectorContextProvider>
   );
